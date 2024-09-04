7749e8e709cb02e713949af6b12e70f03c5e2426
AntonioMelino <antoniomelino1997@gmail.com>
2024-09-04 16:48:59 -0300

    Primera estructura de la ventana principal

--- TPWinForm_equipo-11A/TrabajoPracticoWinForm/Form1.Designer.cs
+++ TPWinForm_equipo-11A/TrabajoPracticoWinForm/Form1.Designer.cs
@@ -28,13 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lwArticulos = new System.Windows.Forms.ListView();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lvlCargaArticulos = new System.Windows.Forms.Label();
+            this.cboMarca = new System.Windows.Forms.ComboBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(143, 107);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(184, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(77, 286);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(250, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lwArticulos
+            // 
+            this.lwArticulos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lwArticulos.HideSelection = false;
+            this.lwArticulos.Location = new System.Drawing.Point(372, 107);
+            this.lwArticulos.Name = "lwArticulos";
+            this.lwArticulos.Size = new System.Drawing.Size(390, 202);
+            this.lwArticulos.TabIndex = 8;
+            this.lwArticulos.UseCompatibleStateImageBehavior = false;
+            this.lwArticulos.View = System.Windows.Forms.View.List;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(97, 107);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(85, 224);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 9;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(97, 198);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 10;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(100, 250);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 12;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(74, 172);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 13;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(97, 137);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 8;
+            this.lblCodigo.Text = "Código";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(143, 137);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(184, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(143, 165);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(184, 20);
+            this.txtDescripcion.TabIndex = 2;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(143, 250);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(184, 20);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // lvlCargaArticulos
+            // 
+            this.lvlCargaArticulos.AutoSize = true;
+            this.lvlCargaArticulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlCargaArticulos.Location = new System.Drawing.Point(71, 23);
+            this.lvlCargaArticulos.Name = "lvlCargaArticulos";
+            this.lvlCargaArticulos.Size = new System.Drawing.Size(234, 31);
+            this.lvlCargaArticulos.TabIndex = 14;
+            this.lvlCargaArticulos.Text = "Carga de articulos";
+            // 
+            // cboMarca
+            // 
+            this.cboMarca.FormattingEnabled = true;
+            this.cboMarca.Location = new System.Drawing.Point(143, 198);
+            this.cboMarca.Name = "cboMarca";
+            this.cboMarca.Size = new System.Drawing.Size(184, 21);
+            this.cboMarca.TabIndex = 3;
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(143, 226);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(184, 21);
+            this.cboCategoria.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(834, 489);
+            this.Controls.Add(this.cboCategoria);
+            this.Controls.Add(this.cboMarca);
+            this.Controls.Add(this.lvlCargaArticulos);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lwArticulos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.MinimumSize = new System.Drawing.Size(850, 528);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ventana principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListView lwArticulos;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lvlCargaArticulos;
+        private System.Windows.Forms.ComboBox cboMarca;
+        private System.Windows.Forms.ComboBox cboCategoria;
     }
 }
 
--- TPWinForm_equipo-11A/TrabajoPracticoWinForm/Form1.cs
+++ TPWinForm_equipo-11A/TrabajoPracticoWinForm/Form1.cs
@@ -16,5 +16,35 @@ namespace TrabajoPracticoWinForm
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboMarca.Items.Add("Samsung");
+            cboMarca.Items.Add("Apple");
+            cboMarca.Items.Add("Sony");
+            cboMarca.Items.Add("Huawei");
+            cboMarca.Items.Add("Motorola");
+
+            cboCategoria.Items.Add("Celulares");
+            cboCategoria.Items.Add("Televisores");
+            cboCategoria.Items.Add("Media");
+            cboCategoria.Items.Add("Audio");
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string articulo = txtNombre.Text;
+            string codigo = txtCodigo.Text;
+            string descripcion = txtDescripcion.Text;
+            string marca = cboMarca.SelectedItem.ToString();
+            string categoria = cboCategoria.SelectedItem.ToString();
+            string precio = txtPrecio.Text;
+            lwArticulos.Items.Add(articulo + " " + codigo + " " + descripcion + " " + marca + " " + categoria + " $" + precio);
+
+            txtNombre.Clear();
+            txtCodigo.Clear();
+            txtDescripcion.Clear();
+            txtPrecio.Clear();
+        }
     }
 }
